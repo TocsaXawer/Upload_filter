eeeea4c916d28974f7f6f75fc75f443eb413b681
TocsaXawer <50368461+TocsaXawer@users.noreply.github.com>
2021-01-31 15:19:54 +0100

    Add files via upload

---
+++ run.py
@@ -0,0 +1,3 @@
+import os
+os.system("gnome-terminal -e 'bash -c \"python3.9 app.py; exec bash\"'")
+os.system("gnome-terminal -e 'bash -c \"python3.9 select_runer.py; exec bash\"'")
---
+++ select_runer.py
@@ -0,0 +1,6 @@
+import time
+import os
+while True: 
+    time.sleep(5)
+    os.system("python3.9 se_tar.py")
+
